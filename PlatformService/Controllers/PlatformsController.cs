c98916dfb35a81110f499a4deecb2d065c05048d
Trieu Nguyen <hoangtrieunguyen.96@gmail.com>
2024-04-26 00:33:39 +1000

    Added CommandsService and a http client to communicate with CommandsService

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Dtos;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService.Controllers;
 
@@ -11,11 +12,16 @@ public class PlatformsController : ControllerBase
 {
     private readonly IPlatformRepo _platformRepo;
     private readonly IMapper _mapper;
+    private readonly ICommandDataClient _commandDataClient;
 
-    public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+    public PlatformsController(
+        IPlatformRepo platformRepo, 
+        IMapper mapper,
+        ICommandDataClient commandDataClient)
     {
         _platformRepo = platformRepo;
         _mapper = mapper;
+        _commandDataClient = commandDataClient;
     }
 
     [HttpGet]
@@ -36,13 +42,22 @@ public class PlatformsController : ControllerBase
     }
 
     [HttpPost]
-    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto platformCreateDto)
     {
         var platformModel = _mapper.Map<Platform>(platformCreateDto);
         _platformRepo.CreatePlatform(platformModel);
         _platformRepo.SaveChanges();
 
         var platformItem = _mapper.Map<PlatformReadDto>(platformModel);
+
+        try
+        {
+            await _commandDataClient.SendPlatformToCommand(platformItem);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+        }
 
         return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformItem.Id}, platformItem);
     }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,25 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
     builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+    builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
     builder.Services.AddAutoMapper(typeof(Program));
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+
+    Console.WriteLine($"--> CommandService endpoint: {builder.Configuration["CommandService"]}");
 }
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
     app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI();
     PrepDb.PrepPopulation(app);
     app.Run();
 }
